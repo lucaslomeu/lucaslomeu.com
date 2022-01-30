4958834593253ad27d23c20b659ea595ba8ea678
lucaslomeu <lucaslomeugomes@gmail.com>
2022-01-30 14:04:34 -0300

    Update Include Flexbox scss

--- src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './Footer.scss';
-
-const Footer = () => {
-  return <footer className="nav-footer">Todos os direitos reservados.</footer>;
-};
-
-export default Footer;
